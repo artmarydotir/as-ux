1351418cea6f3b1d763eab307b5e7e94c25b3d14
Art Mary <artmary.ir@gmail.com>
2020-01-16 02:00:11 +0330

    login without validate.

--- src/components/user/login.vue
+++ src/components/user/login.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card flat color="grey lighten-5">
     <v-card-text>
-      <v-form>
+      <v-form ref="form" v-model="valid" :lazy-validation="lazy">
         <v-text-field
           label="شماره تلفن , ایمیل یا نام کاربری"
           name="login"
@@ -11,24 +11,24 @@
         <v-row>
           <v-col cols="12" md="6">
             <v-text-field
-              id="password"
               label="رمز عبور شخصی"
-              name="password"
+              v-model="pass"
               :append-icon="show1 ? 'mdi-eye-off-outline' : 'mdi-eye'"
               :type="show1 ? 'text' : 'password'"
-              :hint="hint"
+              :hint="hint1"
               @click:append="show1 = !show1"
+              :disabled="otp.length > 0 ? '' : disabled"
             />
           </v-col>
           <v-col cols="12" md="6">
             <v-text-field
-              id="password"
               label="رمز یکبار مصرف"
-              name="password"
+              v-model="otp"
+              :append-icon="show2 ? 'mdi-eye-off-outline' : 'mdi-eye'"
+              :type="show2 ? 'text' : 'password'"
               :hint="hint2"
-              :append-icon="show1 ? 'mdi-eye-off-outline' : 'mdi-eye'"
-              :type="show1 ? 'text' : 'password'"
-              @click:append="show1 = !show1"
+              @click:append="show2 = !show2"
+              :disabled="pass.length > 0 ? '' : disabled"
             />
           </v-col>
         </v-row>
@@ -46,7 +46,7 @@
               height="50" src="../../assets/captcha.png">
             </v-img>
             <v-btn
-              color="deep-orange darken-3"
+              color="error"
               class="py-6 float-left white--text"
             >
               بازیابی
@@ -54,14 +54,12 @@
             </v-btn>
           </v-col>
         </v-row>
-        <h2 class="text-center">
-          و یا از طریق رمز یک بار مصرف وارد شوید
-        </h2>
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary">ورود</v-btn>
+      <v-btn @click="reset" color="warning">لغو</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -72,13 +70,24 @@ export default {
   data() {
     return {
       tab: null,
-      hint: 'معایب استفاده از رمز عبور شخصی کدامند؟',
+      valid: true,
+      disabled: false,
+      hint1: 'معایب استفاده از رمز عبور شخصی کدامند؟',
       hint2: 'چرا باید از رمزیکبار مصرف استفاده کنیم؟',
       show1: false,
+      show2: false,
+      otp: '',
+      pass: '',
+      lazy: false,
       tabTitle: [
         'ورود', 'ثبت نام',
       ],
     };
+  },
+  methods: {
+    reset() {
+      this.$refs.form.reset();
+    },
   },
 };
 </script>
